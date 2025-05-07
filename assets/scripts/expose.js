5865a8a704a2b430d26943b0d6c08403b9f01df1
minnie <minniezhang0510@gmail.com>
2025-05-07 00:12:36 -0700

    fix

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -11,14 +11,15 @@ function init() {
   const volumeIcon = document.querySelector("#volume-controls img")
   const button = document.querySelector("button")
   const confetti = new JSConfetti();
-    hornSelect.addEventListener("change", () =>{
+  
+  hornSelect.addEventListener("change", () =>{
     const selected = hornSelect.value;
     image.src = `assets/images/${selected}.svg`;
     audio.src = `assets/audio/${selected}.mp3`;
-});
+  });
 
 
-slider.addEventListener("input", () =>{
+  slider.addEventListener("input", () =>{
     const volume = slider.value;
     audio.volume = volume / 100;
 
@@ -35,9 +36,9 @@ slider.addEventListener("input", () =>{
     else{
       volumeIcon.src = "assets/icons/volume-level-3.svg";
     }
-});
+  });
 
-button.addEventListener("click", () => {
+  button.addEventListener("click", () => {
     audio.play();
     if(hornSelect.value == "party-horn"){
       confetti.addConfetti();
